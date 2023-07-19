2c4326b06b0d02565c72cd5624a239ea694f224d
gokcesariciyil <gsrcyl@gmail.com>
2023-07-19 15:14:26 +0300

    input prompt & examples

--- App.tsx
+++ App.tsx
@@ -5,16 +5,23 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  Button,
+  Alert,
   useColorScheme,
   View,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TextInput
 } from 'react-native';
 
 import {
@@ -29,90 +36,53 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+// function Section({ children, title }: SectionProps): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+// }
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const [text, setText] = useState("")
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={styles.background}>
+      <TextInput maxLength={20}
+        style={{ width: 200, height: 50, borderWidth: 1 }}
+        placeholder='Type something'
+        value={text}
+        onChangeText={setText}
+        secureTextEntry={true}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text>{text}</Text>
+      <Button title="Prompt" onPress={() => Alert.prompt("Hello " + text, "How are you?", (val) => console.log(val))} />
     </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    marginTop: 50,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  red: {
+    color: 'red',
   },
-  highlight: {
-    fontWeight: '700',
+  image: {
+    width: "auto",
+    height: 200,
+    resizeMode: 'cover',
   },
+  background: {
+    backgroundColor: 'pink',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
+
 
 export default App;
---
+++ components/examples.tsx
@@ -0,0 +1,48 @@
+{/*
+
+  //const isDarkMode = useColorScheme() === 'dark';
+<Text style={[styles.bigBlue]} >
+Lorem ipsum dolor{"\n"}
+Officia,{"\n"}dolores
+</Text>
+<Image style={styles.image} source={require("./download.jpg")} />
+
+<TouchableHighlight onPress={() => console.log("TouchableHighlight!")}>
+<View style={{ width: '100%', height: 75, backgroundColor: 'cyan' }}></View>
+</TouchableHighlight>
+
+
+<TouchableOpacity onPress={() => console.log("TouchableOpacity!")}>
+<View style={{ width: '100%', height: 75, backgroundColor: 'red' }}></View>
+</TouchableOpacity>
+
+
+<TouchableOpacity onPress={() => console.log("TouchableOpacity activeOpacity={1}!")} activeOpacity={1}>
+<View style={{ width: '100%', height: 75, backgroundColor: 'pink' }}></View>
+</TouchableOpacity>
+
+
+<TouchableWithoutFeedback onPress={() => console.log("TouchableWithoutFeedback --> Same With activeOpacity={1}!")}>
+<View style={{ width: '100%', height: 75, backgroundColor: 'blue' }}></View>
+</TouchableWithoutFeedback>
+
+
+<TouchableOpacity onPress={() => console.log("Image inside TouchableOpacity")}>
+<Image style={styles.image} source={require("./download.jpg")} />
+</TouchableOpacity>
+
+
+<Button title='press me'
+onPress={() => Alert.alert("Hello " + text, "How are you?",
+        [
+            { text: "Yes", onPress: () => console.log("Clicked YES") },
+            { text: "No!", onPress: () => console.log("Clicked NO") }
+        ]
+    )}
+/>
+
+
+
+
+
+*/}
